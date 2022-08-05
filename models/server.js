57ebbd8fc576aa73ab21308c8ec9bf3801bbdbd5
Bryan31542 <00063820@uca.edu.sv>
2022-08-05 14:52:57 -0600

    feat: sending and receiving :sparkles:

--- models/server.js
+++ models/server.js
@@ -32,10 +32,12 @@ class Server {
 
   sockets() {
     this.io.on("connection", (socket) => {
-      console.log("Client connected", socket.id);
-
       socket.on("disconnect", () => {
         console.log("Client disconnected", socket.id);
+      });
+
+      socket.on("send-message", (payload) => {
+        this.io.emit("send-message", payload);
       });
     });
   }
--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -2,17 +2,36 @@
 const lblOnline = document.querySelector("#lblOnline");
 const lblOffline = document.querySelector("#lblOffline");
 
+const btnSend = document.querySelector("#btnSend");
+const txtMessage = document.querySelector("#txtMessage");
+
 const socket = io();
 
 socket.on("connect", () => {
-  console.log("Connected");
+  // console.log("Connected");
 
   lblOffline.style.display = "none";
   lblOnline.style.display = "";
 });
 
 socket.on("disconnect", () => {
-  console.log("Disconnected");
+  // console.log("Disconnected");
   lblOffline.style.display = "";
   lblOnline.style.display = "none";
+});
+
+socket.on("send-message", (payload) => {
+  console.log(payload);
+});
+
+btnSend.addEventListener("click", () => {
+  const message = txtMessage.value;
+
+  const payload = {
+    message,
+    id: "123",
+    timestamp: Date.now(),
+  };
+
+  socket.emit("send-message", payload);
 });
