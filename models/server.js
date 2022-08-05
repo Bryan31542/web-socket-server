a5e2577c9406e22eefbfa0c0cee87bce750c84ed
Bryan31542 <00063820@uca.edu.sv>
2022-08-05 13:37:42 -0600

    feat: init project :tada:

---
+++ models/server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const cors = require("cors");
+
+class Server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
+
+    this.paths = {};
+
+    // Middlewares
+    this.middlewares();
+
+    // Routes
+    this.routes();
+  }
+
+  middlewares() {
+    // Cors
+    this.app.use(cors());
+
+    // Public folder
+    this.app.use(express.static("public"));
+  }
+
+  routes() {}
+
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log(`Server running on port ${this.port}`);
+    });
+  }
+}
+
+module.exports = Server;
